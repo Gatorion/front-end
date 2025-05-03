5d988eb89bcc88a7bdfd27d49216f6e3bf0dc22b
Rafael Rios <rafaelrios807@gmail.com>
2025-05-03 01:27:21 -0300

    feat: Adicionado arquivo de renderização e agrupamento de componentes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import React, { useState } from 'react'
 import './App.css'
+import Aside from '/src/components/Aside'
+import SocialMedia from '/src/components/SocialMedia'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
+      <Header />
+      <div className='content'>
+        <Aside />
+        <SocialMedia />
+      </div>
     </>
   )
 }
