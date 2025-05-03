30dbfd2e65c7ef13f2335d84ecee6329101d1d9f
Rafael Rios <rafaelrios807@gmail.com>
2025-05-03 14:51:09 -0300

    refactor: Organizada a renderização do conteúdo no App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Aside from '/src/components/Aside'
 import SocialMedia from '/src/components/SocialMedia'
 import Header from './components/Header'
+import PostImage from './assets/img/PostPicture.png'
 
 function App() {
   return (
@@ -10,7 +11,24 @@ function App() {
       <Header />
       <div className='content'>
         <Aside />
-        <SocialMedia />
+        <div className='posts'>
+          <SocialMedia
+            postText='Finalmente lançamos nossa plataforma! Esperamos que aproveitem ao máximo essa jornada astronômica! 🚀💫'
+            postImage={PostImage}
+            postLikes='1 mil'
+            postComents='175'
+            postShares='14'
+            postStatistics='50 mil'
+          />
+          <SocialMedia
+            postText='Marcha, familia! 🚀💫'
+            postImage={PostImage}
+            postLikes='666 mil'
+            postComents='23 mil'
+            postShares='13 mil'
+            postStatistics='789 mil'
+          />
+         </div>
       </div>
     </>
   )
