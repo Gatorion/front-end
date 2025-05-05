2edf0007dc15a59dbff742d28ce657b6283cfc8a
rafael_rios <dev.rafaelrios807@outlook.com>
2025-05-05 02:03:23 -0300

    refactor: inclui espaçamentos para melhor visualização do código
    - Ajusta espaçamentos no arquivo para melhorar a legibilidade.
    
    - Nenhuma funcionalidade foi alterada.

--- src/components/CardContent/CardContent.jsx
+++ src/components/CardContent/CardContent.jsx
@@ -1,9 +1,9 @@
 import styles from './CardContent.module.css';
 
-const CardContent = ({icon, title, content}) => {
+const CardContent = ({ icon, title, content }) => {
     return (
         <div className={styles.container}>
-            <img src={icon} alt=""/>
+            <img src={icon} alt="" />
             <h2>{title}</h2>
             <p>{content}</p>
         </div>
